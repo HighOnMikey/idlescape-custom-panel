f744ef0e717ea37d7509f390f047363e9e0d395d
HighOnMikey <3639519+HighOnMikey@users.noreply.github.com>
2021-12-08 12:03:45 -0600

    Add color picker element

--- src/custom-panel-settings-ui.js
+++ src/custom-panel-settings-ui.js
@@ -41,4 +41,51 @@ class CustomPanelSettingsUI {
 
         return row;
     }
+
+    static createSettingsColorPicker(description, color, defaultColor, saveCallback = () => {}) {
+        let row = document.createElement("div");
+        row.className = "settings-row";
+
+        let picker = row.appendChild(document.createElement("input"));
+        picker.type = "color";
+        picker.value = color;
+        picker.className = "settings-button";
+        picker.style.backgroundColor = "transparent";
+
+        let save = row.appendChild(document.createElement("div"));
+        save.className = "settings-button";
+        save.style.textAlign = "center";
+        save.onclick = () => saveCallback(picker.value);
+        save.append("Save");
+
+        let reset = row.appendChild(document.createElement("div"));
+        reset.className = "settings-button";
+        reset.style.backgroundColor = "#8B0000";
+        reset.style.textAlign = "center";
+        reset.onclick = () => {
+            picker.value = color;
+            saveCallback(picker.value);
+        };
+        reset.append("Reset");
+
+        let defaultButton = row.appendChild(document.createElement("div"));
+        defaultButton.className = "settings-button";
+        defaultButton.style.backgroundColor = "#8B0000";
+        defaultButton.style.textAlign = "center";
+        defaultButton.onclick = () => {
+            picker.value = defaultColor;
+            saveCallback(picker.value);
+        };
+        defaultButton.append("Default");
+
+        let label = row.appendChild(document.createElement("label"));
+        label.style.height = "28px";
+        label.style.lineHeight = "28px";
+        label.style.marginLeft = "10px";
+        label.style.marginBottom = "0px";
+        label.style.marginTop = "3px";
+        label.append(`${description}`);
+
+        return row;
+    }
 }
